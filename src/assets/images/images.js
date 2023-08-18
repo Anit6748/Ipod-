e8d1de2ecbafb8629f51ebdeba4767c71311dbbc
ANIT Kumar <103092524+Anit6748@users.noreply.github.com>
2023-08-18 13:55:40 +0530

    Add files via upload

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./wallpaper1.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import games from "./games.jpeg";
+import allsongs from "./allsongs.gif";
+import artists from "./artists.jpg";
+import albums from "./albums.png";
+import song1Img from "./amplifier.png";
+import song2Img from "./allblack.jpeg";
+import song3Img from "./mrx.png";
+
+const images = {
+    wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+
+    games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
+
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,10 @@
+import music1 from "./amplifier.mp3";
+import music2 from "./allblack.mp3";
+import music3 from "./mrx.mp3";
+
+const songs = {
+    music1,
+    music2,
+    music3,
+};
+export default songs;
